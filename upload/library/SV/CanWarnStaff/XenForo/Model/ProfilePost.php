bd2d7ab10e5b25f69fe70303103c2dd4e0dd8266
Xon <xon@home.local>
2014-09-25 22:10:31 +0800

    typo fix

--- upload/library/SV/CanWarnStaff/XenForo/Model/ProfilePost.php
+++ upload/library/SV/CanWarnStaff/XenForo/Model/ProfilePost.php
@@ -10,12 +10,12 @@ class SV_CanWarnStaff_XenForo_Model_ProfilePost extends XFCP_SV_CanWarnStaff_Xen
             return true;
         }
 
-        if ($post['warning_id'] || empty($post['user_id']))
+        if ($profilePost['warning_id'] || empty($profilePost['user_id']))
         {
             return false;
         }
        
-        if (!empty($post['is_admin']) && $post['is_admin'])
+        if (!empty($profilePost['is_admin']) && $profilePost['is_admin'])
         {
             // allow moderators & admins if they have the right permission
             $this->standardizeViewingUserReference($viewingUser);
@@ -23,7 +23,7 @@ class SV_CanWarnStaff_XenForo_Model_ProfilePost extends XFCP_SV_CanWarnStaff_Xen
             return ($viewingUser['user_id'] && XenForo_Permission::hasPermission($viewingUser['permissions'], 'general', 'warn_admin'));
         }
         
-        if (!empty($post['is_moderator']) && $post['is_moderator'])
+        if (!empty($profilePost['is_moderator']) && $profilePost['is_moderator'])
         {
             // allow moderators & admins if they have the right permission
             $this->standardizeViewingUserReference($viewingUser);
